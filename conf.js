67cb2dbfaa29497c01ef91d28c78b8d7a6be5439
mdsauce <max.dobeck@saucelabs.com>
2018-06-12 15:54:49 -0700

    Barebones test that connects to RDC.

---
+++ conf.js
@@ -0,0 +1,56 @@
+var TestObject = require('./node_modules/testobject_api/lib/TestObject');
+
+exports.config = {
+
+    // seleniumAddress: 'http://eu1.appium.testobject.com/wd/hub',
+    seleniumAddress: 'http://us1.appium.testobject.com/wd/hub',
+    specs: ['specs/*spec.js'],
+
+    onPrepare: function() {
+        var caps = browser.getCapabilities();
+    },
+
+    multiCapabilities: [{
+        testobject_api_key: process.env.apricot_api,
+        testobject_device: 'Google_Pixel_real',
+        testobject_test_name: Date.now() + " - Testing screenshots",
+        testobject_cache_device: "true",
+        browserName: 'chrome',
+        shardTestFiles: true,
+        maxInstances: 3,
+        autoWebview : true,
+        autoWebviewTimeout: 12000,
+    }, {
+        testobject_api_key: process.env.apricot_api,
+        testobject_test_name: Date.now() + " - Testing screenshots",
+        testobject_cache_device: "false",
+        platformName: "Android",
+        platformVersion: "7",
+        phoneOnly: "true",
+        browserName: 'chrome',
+        shardTestFiles: true,
+        maxInstances: 3,
+        autoWebview : true,
+        autoWebviewTimeout: 12000,
+    },
+    ],
+
+    onComplete: function(result) {
+        var myAccount = new TestObject({
+            username: 'csteam',
+            apiKey: process.env.apricot_api,
+            password: process.env.RDC_ADMIN_PW
+        });
+
+        console.log("result is " + result);
+
+        var printSessionId = function(jobName) {
+            browser.getSession().then(function(session) {
+                console.log('SauceOnDemandSessionID=' + session.getId() + ' job-name=' + jobName);
+                    myAccount.updateTest(session.getId(), {"passed": result}, function(res){
+                });
+            });
+        }
+        printSessionId("Screenshot Tests");
+    }
+};
---
+++ specs/initial-screenshot-spec.js
@@ -0,0 +1,15 @@
+let fs = require('fs')
+
+function writeScreenShot(data, filename) {
+    var stream = fs.createWriteStream(filename);
+    stream.write(new Buffer(data, 'base64'));
+    stream.end();
+}
+
+describe('Grab initial screenshot of Login page', () => {
+    browser.context('NATIVE_APP').then(() => {
+        browser.takeScreenshot().then(function (png){
+            writeScreenShot(png, 'loginScreenshot.png')
+        })
+    })
+})
