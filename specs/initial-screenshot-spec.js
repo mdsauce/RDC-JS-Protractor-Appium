5cfe17620997ad16e49abd5569e340cd01ee2d00
mdsauce <max.dobeck@saucelabs.com>
2018-06-13 13:02:55 -0700

    Trying async await strategy.

--- specs/initial-screenshot-spec.js
+++ specs/initial-screenshot-spec.js
@@ -8,10 +8,10 @@ function writeScreenShot(data, filename) {
 
 describe('When my app loads', function () {
     console.log("Now trying to take a screenshot")
-    it('will take a screenshot of the login', function () {
+    it('will take a screenshot of the login', async function () {
         console.log("The screenshot is about to start <<<< 5, 4, 3, 2, 1...")
         browser.driver.sleep(7000)
-        browser.takeScreenshot().then(function (png){
+        await browser.takeScreenshot().then(function (png){
             console.log("inside the screenshot function <--------<----------<-----")
             writeScreenShot(png, 'loginScreenshot.png')
         })
