ef5507a34e7e4933ad2659d8242ea80fd4112c5f
mdsauce <max.dobeck@saucelabs.com>
2018-06-13 13:06:28 -0700

    adding more await

--- specs/initial-screenshot-spec.js
+++ specs/initial-screenshot-spec.js
@@ -11,9 +11,9 @@ describe('When my app loads', function () {
     it('will take a screenshot of the login', async function () {
         console.log("The screenshot is about to start <<<< 5, 4, 3, 2, 1...")
         browser.driver.sleep(7000)
-        await browser.takeScreenshot().then(function (png){
+        await browser.takeScreenshot().then(async function (png){
             console.log("inside the screenshot function <--------<----------<-----")
-            writeScreenShot(png, 'loginScreenshot.png')
+            await writeScreenShot(png, 'loginScreenshot.png')
         })
     })
 })
